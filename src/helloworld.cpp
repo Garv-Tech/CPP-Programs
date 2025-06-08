4520c6b76a8a86553c9df037507578b42009beea
Garv <garvsachan.tech@gmail.com>
2025-06-08 16:05:08 +0000

    Updated directory management

---
+++ src/helloworld.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(){
+
+   // This is a comment
+   /* This is a multiline comment
+      Comments are ignored by the compiler
+   */   
+   
+   std::cout << "Hello, World!\n";
+   std::cout << "They are too good for me!\n";
+
+   return 0;
+  
+}
